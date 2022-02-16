9752bfb65e100205f52b7988b9e06147a2ad59eb
Kaynen Heikkinen <kaynenheikkinen@macbook-air.lan>
2022-02-15 23:13:08 -0600

    Skeleton of app

---
+++ src/routes/stats.svelte
@@ -0,0 +1,31 @@
+<script>
+	import Header from '$lib/components/header.svelte';
+	import Nav from '$lib/components/nav.svelte';
+  import Chart from 'svelte-frappe-charts';
+
+  let data = {
+    labels: ['July 2', 'July 9', 'July 16', 'July 23', 'July 30', 'Aug 5', 'Aug 12'],
+    datasets: [
+      {
+        values: [39, 41, 40, 36, 38, 34, 35]
+      }
+    ]
+  };
+</script>
+
+
+<div>
+<Nav />
+<Header title='Stats' />
+<main>
+  <div class="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
+    <!-- Replace with your content -->
+    <div class="px-4 py-6 sm:px-0">
+      <div class="border-4 border-dashed border-gray-200 rounded-lg h-96">
+        <Chart data={data} type="bar" />
+      </div>
+    </div>
+    <!-- /End replace -->
+  </div>
+</main>
+</div>
